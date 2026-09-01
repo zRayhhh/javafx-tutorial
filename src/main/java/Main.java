d7ed3c766d66bc44074d702c84f12b8086fa00cc
zRayhhh <150665647+zRayhhh@users.noreply.github.com>
2026-09-01 15:54:58 +0800

    Add Tutorial 5 auto resize and CSS elements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
